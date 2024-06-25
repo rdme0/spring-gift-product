4fcc36c2530362d942e9c7ccee9a401f52bca461
김수랑 <we6610@gmail.com>
2024-06-25 19:15:25 +0900

    feat: 기본적인 CRUD 구현

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -1,2 +1,52 @@
-package gift.Controller;public class ProductController {
+package gift.controller;
+
+import gift.domain.ProductDTO;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+    private final List<ProductDTO> products = new ArrayList<>();
+
+    @GetMapping("/products")
+    public List<Map<Integer, ProductDTO>> getProducts() {
+        List<Map<Integer, ProductDTO>> productsListForOutput = new ArrayList<>();
+        listIndexToKey(products, productsListForOutput);
+        return productsListForOutput;
+    }
+
+    @PostMapping("/products")
+    public void addProduct(@RequestBody ProductDTO productDTO) {
+        products.add(productDTO);
+    }
+
+    @DeleteMapping("/products/{id}")
+    public void deleteProduct(@PathVariable Integer id) {
+        System.out.println(products.get(id - 1));
+        products.remove(id-1);
+    }
+
+    @PutMapping("products/{id}")
+    public void updateProduct(@PathVariable Integer id, @RequestBody ProductDTO productDTO) {
+        products.set((int) (id-1), productDTO);
+    }
+
+    private void listIndexToKey(List<ProductDTO> products, List<Map<Integer, ProductDTO>> productMaps) {
+        Iterator<ProductDTO> iterator = products.iterator();
+        for (int i = 0; iterator.hasNext(); i++) {
+            Map<Integer, ProductDTO> map = new HashMap<>();
+            map.put((i + 1), iterator.next());
+            productMaps.add(map);
+        }
+    }
 }
