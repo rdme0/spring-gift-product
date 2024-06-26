863697265b3ebf1271ee4c7e3e151ad3191754c0
김수랑 <we6610@gmail.com>
2024-06-26 15:12:15 +0900

    :white_check_mark: feat: post 요청 테스트코드 추가

--- src/test/java/gift/controller/ProductControllerTest.java
+++ src/test/java/gift/controller/ProductControllerTest.java
@@ -30,6 +30,17 @@ class ProductControllerTest {
 
     @Test
     void addProductTest() throws Exception {
+
+        String requestJson = "{\n"
+                + "    \"id\": 8146028,\n"
+                + "    \"name\": \"아이스 카페 아메리카노\",\n"
+                + "    \"price\": 4500,\n"
+                + "    \"imageUrl\": \"https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg\"\n"
+                + "}";
+
+        mockMvc.perform(post("/products").content(requestJson).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 
     @Test
