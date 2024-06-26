15bbd0dc6f1b0c43853c9773ee599a45b415bebd
김수랑 <we6610@gmail.com>
2024-06-26 17:23:28 +0900

    :bug: fix: 일부 메서드를 String에서 -> json으로 응답하게 수정 및 자잘한 수정

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -2,12 +2,12 @@ package gift.controller;
 
 import gift.customException.CustomException;
 import gift.domain.ProductDTO;
+import gift.domain.ResponseDTO;
 import gift.model.ProductModel;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,61 +23,59 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Validated
 public class ProductController {
-
-    private final ProductUtil productUtil;
     private final ProductModel productModel;
+    private final String CRITICAL_ERROR_MESSAGE = "확인되지 않은 에러입니다. 관리자에게 문의 주세요";
+    private final String WELL_DONE_MESSAGE = "Success";
 
     @Autowired //생성자 주입
-    public ProductController(ProductUtil productUtil, ProductModel productModel) {
-        this.productUtil = productUtil;
+    public ProductController(ProductModel productModel) {
         this.productModel = productModel;
     }
 
     @GetMapping("/products")
-    public List<Map<Integer, ProductDTO>> getProducts() {
-        return productUtil.listIndexToKey(
-                productModel.getProducts()); //리스트의 인덱스를 map의 Key + 1로 변환해주는 메서드
+    public List< ProductDTO> getProducts() {
+        return productModel.getProductList(); //리스트의 인덱스를 map의 Key + 1로 변환해주는 메서드
     }
 
     @PostMapping("/products")
-    public ResponseEntity<String> addProduct(@RequestBody @Valid ProductDTO productDTO) {
+    public ResponseEntity<ResponseDTO> addProduct(@RequestBody @Valid ProductDTO productDTO) {
         try {
             productModel.addProduct(productDTO);
         } catch (RuntimeException e) {
             if(e instanceof CustomException) {
-                return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+                return new ResponseEntity<>(new ResponseDTO(true, e.getMessage()), HttpStatus.BAD_REQUEST);
             }
-            return new ResponseEntity<>("확인되지 않은 에러입니다. 관리자에게 문의 주세요", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(new ResponseDTO(true, CRITICAL_ERROR_MESSAGE), HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return new ResponseEntity<>("Success", HttpStatus.CREATED);
+        return new ResponseEntity<>(new ResponseDTO(false, WELL_DONE_MESSAGE), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/products/{id}")
-    public ResponseEntity<String> deleteProduct(@PathVariable @Min(1) @NotNull Integer id) {
+    public ResponseEntity<ResponseDTO> deleteProduct(@PathVariable @Min(1) @NotNull Integer id) {
         try{
-            productModel.deleteProduct(id-1);
+            productModel.deleteProduct(id);
         } catch (RuntimeException e) {
             if (e instanceof CustomException) {
-                return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+                return new ResponseEntity<>(new ResponseDTO(true, e.getMessage()), HttpStatus.BAD_REQUEST);
             }
-            return new ResponseEntity<>("확인되지 않은 에러입니다. 관리자에게 문의 주세요", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(new ResponseDTO(true, CRITICAL_ERROR_MESSAGE), HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return new ResponseEntity<>("Success", HttpStatus.OK);
+        return new ResponseEntity<>(new ResponseDTO(false, WELL_DONE_MESSAGE), HttpStatus.OK);
     }
 
 
     @PutMapping("/products/{id}")
-    public ResponseEntity<String> updateProduct(@PathVariable  @Min(1) @NotNull Integer id, @RequestBody @Valid ProductDTO productDTO) {
+    public ResponseEntity<ResponseDTO> updateProduct(@PathVariable  @Min(1) @NotNull Integer id, @RequestBody @Valid ProductDTO productDTO) {
         try {
-            productModel.updateProduct(id - 1, productDTO);
+            productModel.updateProduct(id, productDTO);
 
         } catch (RuntimeException e) {
             if (e instanceof CustomException) {
-                return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+                return new ResponseEntity<>(new ResponseDTO(true, e.getMessage()), HttpStatus.BAD_REQUEST);
             }
-            return new ResponseEntity<>("확인되지 않은 에러입니다. 관리자에게 문의 주세요", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(new ResponseDTO(true, CRITICAL_ERROR_MESSAGE), HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return new ResponseEntity<>("Success", HttpStatus.OK);
+        return new ResponseEntity<>(new ResponseDTO(false, WELL_DONE_MESSAGE), HttpStatus.OK);
     }
 
 }
