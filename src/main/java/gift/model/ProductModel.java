424b43f5fc080d9663ee86c9a24cc330c94d9705
김수랑 <we6610@gmail.com>
2024-06-26 17:20:04 +0900

    :recycle: refactor: model 객체 안정성을 높이고, 인덱스가 아닌 제품 id로 참조하도록 리팩터링

--- src/main/java/gift/model/ProductModel.java
+++ src/main/java/gift/model/ProductModel.java
@@ -1,51 +1,63 @@
 package gift.model;
 
 import gift.customException.DuplicatedProductIdException;
-import gift.customException.NotFoundSuchIndexException;
+import gift.customException.InvalidIdException;
+import gift.customException.NotFoundSuchProductIdException;
 import gift.domain.ProductDTO;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository //DB 시뮬레이션
 public class ProductModel {
 
-    private final List<ProductDTO> products = new ArrayList<>(); //DB 시뮬레이션
+    private final List<ProductDTO> productList = new ArrayList<>(); //DB 시뮬레이션
 
-    public void addProduct(ProductDTO product) throws DuplicatedProductIdException { //C
-        checkDuplicate(product);
-        products.add(product);
+    public synchronized void addProduct(ProductDTO product) throws DuplicatedProductIdException { //C
+        if(isDuplicateId(product))
+            throw new DuplicatedProductIdException("이미 있는 상품 id 입니다.");
+        productList.add(product);
     }
 
-    public List<ProductDTO> getProducts() { //R
-        return products;
+    public synchronized  List<ProductDTO> getProductList() { //R
+        return new ArrayList<>(productList); //방어적 복사
     }
 
-    public void updateProduct(Integer index, ProductDTO product)
-            throws NotFoundSuchIndexException, DuplicatedProductIdException { //U
+    public synchronized void updateProduct(Integer id, ProductDTO product)
+            throws NotFoundSuchProductIdException, InvalidIdException { //U
 
-        if (products.size() < index + 1) {
-            throw new NotFoundSuchIndexException("%d번째 상품은 존재하지 않습니다.".formatted(index + 1));
+        if (!Objects.equals(product.getId(), id)) {
+            throw new InvalidIdException("올바르지 않은 id입니다.");
         }
-        checkDuplicate(product);
-        products.set(index, product);
 
-    }
+        Optional<ProductDTO> optionalProduct = productList.stream()
+                .filter(p -> id.equals(p.getId()))
+                .findFirst();
 
-    public void deleteProduct(Integer index) throws NotFoundSuchIndexException { //D
-        if (products.size() < index + 1) {
-            throw new NotFoundSuchIndexException("%d번째 상품은 존재하지 않습니다.".formatted(index + 1));
+        if (optionalProduct.isEmpty()) {
+            throw new NotFoundSuchProductIdException("id가 %d인 상품은 존재하지 않습니다.".formatted(id));
         }
-        products.remove(index.intValue());
+        int index = productList.indexOf(optionalProduct.get());
+        productList.set(index, product);
+    }
+
+    public synchronized void deleteProduct(Integer id) throws NotFoundSuchProductIdException { //D
+        boolean removed = productList.removeIf(productDTO -> productDTO.getId().equals(id));
+
+        if(!removed)
+            throw new NotFoundSuchProductIdException("id가 %d인 상품은 존재하지 않습니다.".formatted(id));
     }
 
 
-    private void checkDuplicate(ProductDTO product) {
-        for (ProductDTO productDTO : products) {
-            if (productDTO.getId() == product.getId()) {
-                throw new DuplicatedProductIdException("이미 있는 상품 id 입니다.");
+    private synchronized boolean isDuplicateId(ProductDTO product) {
+        for (ProductDTO productDTO : productList) {
+            if (Objects.equals(productDTO.getId(), product.getId())) {
+                return true;
             }
         }
+        return false;
     }
 
 
