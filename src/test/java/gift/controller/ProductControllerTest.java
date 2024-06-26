ffdd9b5361c0e20a7eecae875d63990bf5be157c
김수랑 <we6610@gmail.com>
2024-06-26 14:20:40 +0900

    :white_check_mark: feat: 테스트 코드 뼈대 추가

---
+++ src/test/java/gift/controller/ProductControllerTest.java
@@ -0,0 +1,24 @@
+package gift.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProductControllerTest {
+
+    @Test
+    void getProducts() {
+    }
+
+    @Test
+    void addProduct() {
+    }
+
+    @Test
+    void deleteProduct() {
+    }
+
+    @Test
+    void updateProduct() {
+    }
+}
