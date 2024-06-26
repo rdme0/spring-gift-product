dc90705e81eb8dea83c4f9a032cbbc1a7653899f
김수랑 <we6610@gmail.com>
2024-06-26 17:18:05 +0900

    :sparkles: feat: 응답을 위한 객체 DTO 추가

--- src/main/java/gift/domain/ResponseDTO.java
+++ src/main/java/gift/domain/ResponseDTO.java
@@ -1,5 +1,27 @@
 package gift.domain;
 
 public class ResponseDTO {
+    boolean isError;
+    String message;
 
+    public ResponseDTO(boolean isError, String message) {
+        this.isError = isError;
+        this.message = message;
+    }
+
+    public boolean isError() {
+        return isError;
+    }
+
+    public void setError(boolean isError) {
+        this.isError = isError;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
