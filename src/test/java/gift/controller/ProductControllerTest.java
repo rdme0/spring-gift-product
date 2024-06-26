e8ff744ea8a61f7f6838ea05eab8609e92a22c65
김수랑 <we6610@gmail.com>
2024-06-26 17:21:18 +0900

    :white_check_mark:  feat: delete와 put 요청에 대한 테스트 코드 추가

--- src/test/java/gift/controller/ProductControllerTest.java
+++ src/test/java/gift/controller/ProductControllerTest.java
@@ -1,13 +1,13 @@
 package gift.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import gift.model.ProductModel;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -30,9 +30,8 @@ class ProductControllerTest {
 
     @Test
     void addProductTest() throws Exception {
-
         String requestJson = "{\n"
-                + "    \"id\": 8146028,\n"
+                + "    \"id\": 1,\n"
                 + "    \"name\": \"아이스 카페 아메리카노\",\n"
                 + "    \"price\": 4500,\n"
                 + "    \"imageUrl\": \"https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg\"\n"
@@ -44,10 +43,56 @@ class ProductControllerTest {
     }
 
     @Test
-    void deleteProduct() {
+    void deleteProductTest() throws Exception {
+        String requestJson = "{\n"
+                + "    \"id\": 2,\n"
+                + "    \"name\": \"아이스 카페 아메리카노\",\n"
+                + "    \"price\": 4500,\n"
+                + "    \"imageUrl\": \"https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg\"\n"
+                + "}";
+        mockMvc.perform(post("/products").content(requestJson).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+        mockMvc.perform(delete("/products/2"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+        mockMvc.perform(post("/products").content(requestJson).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+        mockMvc.perform(delete("/products/412312412"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 
     @Test
-    void updateProduct() {
+    void updateProductTest() throws Exception {
+        String requestJson = "{\n"
+                + "    \"id\": 3,\n"
+                + "    \"name\": \"아이스 카페 아메리카노\",\n"
+                + "    \"price\": 4500,\n"
+                + "    \"imageUrl\": \"https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg\"\n"
+                + "}";
+
+        String putRequestJson = "{\n"
+                + "    \"id\": 3,\n"
+                + "    \"name\": \"아이스 카페 라떼\",\n"
+                + "    \"price\": 1241213,\n"
+                + "    \"imageUrl\": \"https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg\"\n"
+                + "}";
+
+        mockMvc.perform(post("/products").content(requestJson).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+        mockMvc.perform(put("/products/3").content(putRequestJson).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+        mockMvc.perform(put("/products/44324235").content(putRequestJson).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 }
