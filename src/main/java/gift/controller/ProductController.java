818f8ba6208307a98cf4116177bdad623f2db057
김수랑 <we6610@gmail.com>
2024-06-26 13:16:42 +0900

    :sparkles: feat: 컨트롤러, 모델의 예외처리 기능 추가

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -1,9 +1,13 @@
 package gift.controller;
 
+import gift.customException.CustomException;
 import gift.domain.ProductDTO;
-import java.util.ArrayList;
+import gift.model.ProductModel;
+import jakarta.validation.Valid;
 import java.util.List;
 import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,28 +18,58 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
-    private final List<ProductDTO> products = new ArrayList<>();
-    public ProductUtil productUtil = new ProductUtil();
+
+    ProductUtil productUtil = new ProductUtil();
+    ProductModel productModel = new ProductModel();
+
 
     @GetMapping("/products")
     public List<Map<Integer, ProductDTO>> getProducts() {
-        return productUtil.listIndexToKey(products); //리스트의 인덱스를 map의 Key + 1로 변환해주는 메서드
+        return productUtil.listIndexToKey(
+                productModel.getProducts()); //리스트의 인덱스를 map의 Key + 1로 변환해주는 메서드
     }
 
     @PostMapping("/products")
-    public void addProduct(@RequestBody ProductDTO productDTO) {
-        products.add(productDTO);
+    public ResponseEntity<String> addProduct(@RequestBody @Valid ProductDTO productDTO) {
+        try {
+            productModel.addProduct(productDTO);
+        } catch (RuntimeException e) {
+            if(e instanceof CustomException) {
+                return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+            }
+            return new ResponseEntity<>("확인되지 않은 에러입니다. 관리자에게 문의 주세요", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>("Success", HttpStatus.OK);
     }
 
     @DeleteMapping("/products/{id}")
-    public void deleteProduct(@PathVariable Integer id) {
-        System.out.println(products.get(id - 1));
-        products.remove(id-1);
+    public ResponseEntity<String> deleteProduct(@PathVariable @Valid Integer id) {
+        if (id == null || id < 0)
+            return new ResponseEntity<>("Invalid id", HttpStatus.BAD_REQUEST);
+        try{
+            productModel.deleteProduct(id-1);
+        } catch (RuntimeException e) {
+            if (e instanceof CustomException) {
+                return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+            }
+            return new ResponseEntity<>("확인되지 않은 에러입니다. 관리자에게 문의 주세요", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>("Success", HttpStatus.OK);
     }
 
+
     @PutMapping("products/{id}")
-    public void updateProduct(@PathVariable Integer id, @RequestBody ProductDTO productDTO) {
-        products.set(id-1, productDTO);
+    public ResponseEntity<String> updateProduct(@PathVariable Integer id, @RequestBody @Valid ProductDTO productDTO) {
+        try {
+            productModel.updateProduct(id - 1, productDTO);
+
+        } catch (RuntimeException e) {
+            if (e instanceof CustomException) {
+                return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+            }
+            return new ResponseEntity<>("확인되지 않은 에러입니다. 관리자에게 문의 주세요", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>("Success", HttpStatus.OK);
     }
 
 }
--- src/main/java/gift/model/ProductModel.java
+++ src/main/java/gift/model/ProductModel.java
@@ -1,5 +1,52 @@
 package gift.model;
 
+import gift.customException.DuplicatedProductIdException;
+import gift.customException.NotFoundSuchIndexException;
+import gift.domain.ProductDTO;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class ProductModel {
+
+    private final List<ProductDTO> products = new ArrayList<>();
+
+    public void addProduct(ProductDTO product) throws DuplicatedProductIdException { //C
+        checkDuplicate(product);
+        products.add(product);
+    }
+
+    public List<ProductDTO> getProducts() { //R
+        return products;
+    }
+
+    public void updateProduct(Integer index, ProductDTO product)
+            throws NotFoundSuchIndexException, DuplicatedProductIdException { //U
+
+        if (products.size() < index + 1) {
+            throw new NotFoundSuchIndexException("%d번째 상품은 존재하지 않습니다.".formatted(index + 1));
+        }
+        checkDuplicate(product);
+        products.set(index, product);
+
+    }
+
+    public void deleteProduct(Integer index) throws NotFoundSuchIndexException { //D
+        if (products.size() < index + 1) {
+            throw new NotFoundSuchIndexException("%d번째 상품은 존재하지 않습니다.".formatted(index + 1));
+        }
+        products.remove(index.intValue());
+    }
+
+
+    private void checkDuplicate(ProductDTO product) {
+        for (ProductDTO productDTO : products) {
+            if (productDTO.getId() == product.getId()) {
+                throw new DuplicatedProductIdException("이미 있는 상품 id 입니다.");
+            }
+        }
+    }
+
 
 }
