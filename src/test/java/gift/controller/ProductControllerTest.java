6937b57cefd0baa431f3a1935594cbf172c31faf
김수랑 <we6610@gmail.com>
2024-06-26 15:12:00 +0900

    :white_check_mark: feat: get 요청 테스트코드 추가

--- src/test/java/gift/controller/ProductControllerTest.java
+++ src/test/java/gift/controller/ProductControllerTest.java
@@ -1,17 +1,35 @@
 package gift.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import gift.model.ProductModel;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 class ProductControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void getProducts() {
+    void getProductsTest() throws Exception {
+        mockMvc.perform(get("/products"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 
     @Test
-    void addProduct() {
+    void addProductTest() throws Exception {
     }
 
     @Test
