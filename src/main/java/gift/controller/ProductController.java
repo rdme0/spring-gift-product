77e77d12a2bd7f54cd7934275496395c83f37c23
김수랑 <we6610@gmail.com>
2024-06-27 15:00:30 +0900

    :ambulance: fix: uri (/products) -> (/api/products) 로 수정

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -18,10 +18,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Validated
+@RequestMapping("/api")
 public class ProductController {
 
     private final ProductModel productModel;
