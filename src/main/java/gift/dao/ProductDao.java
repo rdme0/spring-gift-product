b087efeee1c588ba59fa7a8b314ca012ef75e216
rdme0 <we6610@gmail.com>
2024-06-28 00:58:57 +0900

    :bug: fix: sql문이 작동 안 하는 버그 수정

--- src/main/java/gift/dao/ProductDao.java
+++ src/main/java/gift/dao/ProductDao.java
@@ -6,11 +6,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ProductDao {
+public class ProductDao implements CommandLineRunner {
     private final JdbcTemplate jdbcTemplate;
 
     @Autowired
@@ -18,25 +19,30 @@ public class ProductDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        var sqlDropTable = "DROP TABLE IF EXISTS product";
+
+        var sqlCreateTable = """
+            CREATE TABLE product (
+              id INT PRIMARY KEY AUTO_INCREMENT,
+              name VARCHAR(255) NOT NULL,
+              price INT NOT NULL,
+              imageUrl VARCHAR(4096) NOT NULL
+            );
+            """;
+
+        jdbcTemplate.execute(sqlDropTable);
+        jdbcTemplate.execute(sqlCreateTable);
+    }
+
 
     public Connection getConnection() throws Exception {
         return DriverManager.getConnection("jdbc:h2:mem:gift", "sa", "");
     }
 
-    public void createProductTable() {
-        var sql = """
-            CREATE TABLE product (
-              id INT PRIMARY KEY AUTO_INCREMENT,
-              name VARCHAR(255) NOT NULL,
-              price INT NOT NULL,
-              imageUrl VARCHAR(4096) NOT NULL,
-            )
-            """;
-        jdbcTemplate.execute(sql);
-    }
-
     public void insertProduct(ProductDTO productDTO) {
-        var sql = "INSERT INTO product (price, name, imageUrl) values (?, ?, ?)";
+        var sql = "INSERT INTO product (name, price, imageUrl) values (?, ?, ?)";
         jdbcTemplate.update(sql, productDTO.getName(), productDTO.getPrice(), productDTO.getImageUrl());
     }
 
@@ -66,5 +72,6 @@ public class ProductDao {
         var sql = "SELECT COUNT(id) FROM product";
         return jdbcTemplate.queryForObject(sql, Integer.class);
     }
+
 
 }
