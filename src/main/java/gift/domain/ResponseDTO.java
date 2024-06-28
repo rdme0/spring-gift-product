108dde3b4e3cdccea3c9a1d9b61ffc6bbb3d6ed8
김수랑 <we6610@gmail.com>
2024-06-28 13:35:04 +0900

    :recycle: refactor(ResponseDTO): class -> record로 리팩터링

--- src/main/java/gift/domain/ResponseDTO.java
+++ src/main/java/gift/domain/ResponseDTO.java
@@ -1,28 +1,3 @@
 package gift.domain;
 
-public class ResponseDTO {
-
-    boolean isError;
-    String message;
-
-    public ResponseDTO(boolean isError, String message) {
-        this.isError = isError;
-        this.message = message;
-    }
-
-    public boolean isError() {
-        return isError;
-    }
-
-    public void setError(boolean isError) {
-        this.isError = isError;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-}
+public record ResponseDTO(boolean isError, String message) { }
