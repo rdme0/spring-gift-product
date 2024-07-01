284bc9d740f8dbaf7b84075b62bd8d3f5de3fa4a
김수랑 <we6610@gmail.com>
2024-07-01 14:31:59 +0900

    :recycle: refactor: 상수에 static 키워드 추가

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -27,8 +27,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProductController {
 
     private final ProductService productService;
-    private final String CRITICAL_ERROR_MESSAGE = "확인되지 않은 에러입니다. 관리자에게 문의 주세요";
-    private final String WELL_DONE_MESSAGE = "Success";
+    private static final String CRITICAL_ERROR_MESSAGE = "확인되지 않은 에러입니다. 관리자에게 문의 주세요";
+    private static final String WELL_DONE_MESSAGE = "Success";
 
     @Autowired //생성자 주입
     public ProductController(ProductService productService) {
